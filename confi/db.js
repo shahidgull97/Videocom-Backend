81d137d2d5682d2a604126fdb35f41a5d183d5b1
Shahid Gull <parrayshahid97@gmail.com>
2025-02-12 21:45:49 +0530

    fixed db connection

--- confi/db.js
+++ confi/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-const baseUrl = process.env.MONGODB || "0.0.0.0:27017";
+
 export const connectDB = async () => {
   try {
     console.log("db is connecting...");
-    const res = await mongoose.connect(`mongodb://${baseUrl}/videocall`);
+    const res = await mongoose.connect(process.env.mongoURI, {
+      maxPoolSize: 10,
+    });
     console.log(`mongodb connected with server ${res.connection.host}`);
   } catch (error) {
     console.log("mongodb connection failed!");
